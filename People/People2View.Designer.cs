9b1f4bf928659f816cf9c3f230ec3a50ab702943
Steve Bachochin <steve.bachochin@gmail.com>
2019-03-31 11:32:29 -0400

    sdf

--- People/People2View.Designer.cs
+++ People/People2View.Designer.cs
@@ -30,20 +30,35 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonClear = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(83, 47);
+            this.dataGridView1.Location = new System.Drawing.Point(83, 166);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(504, 259);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Size = new System.Drawing.Size(478, 140);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            this.dataGridView1.RowLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowLeave);
             // 
             // buttonClear
             // 
-            this.buttonClear.Location = new System.Drawing.Point(677, 73);
+            this.buttonClear.Location = new System.Drawing.Point(594, 45);
             this.buttonClear.Name = "buttonClear";
             this.buttonClear.Size = new System.Drawing.Size(75, 23);
             this.buttonClear.TabIndex = 1;
@@ -51,16 +66,105 @@
             this.buttonClear.UseVisualStyleBackColor = true;
             this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(83, 45);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtLast);
+            this.splitContainer1.Panel2.Controls.Add(this.txtFirst);
+            this.splitContainer1.Size = new System.Drawing.Size(478, 100);
+            this.splitContainer1.SplitterDistance = 159;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // txtLast
+            // 
+            this.txtLast.Location = new System.Drawing.Point(4, 30);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(191, 20);
+            this.txtLast.TabIndex = 1;
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(4, 4);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(191, 20);
+            this.txtFirst.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(594, 71);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(594, 101);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(594, 131);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
             // People2View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.dataGridView1);
             this.Name = "People2View";
-            this.Text = "People2Vw";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -69,5 +173,13 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
--- People/People2View.cs
+++ People/People2View.cs
@@ -13,6 +13,9 @@ namespace People
 {
     public partial class People2View : Form
     {
+        int ID = 0;
+        int ROW = -1;
+
         public People2View()
         {
             InitializeComponent();
@@ -69,5 +72,224 @@ namespace People
             dataGridView1.Rows.Clear();
             dataGridView1.Refresh();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            addName(txtFirst.Text, txtLast.Text);
+            //MessageBox.Show(txtLast.Text);
+        }
+
+        public void clearData()
+        {
+            txtFirst.Text = "";
+            txtLast.Text = "";
+        }
+        /// <summary>
+        /// ADD NAME
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        public void addName(string firstName, string lastName)
+        {
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.PeopleConnectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("addName", connection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    try
+                    {
+                        sqlCommand.Parameters.AddWithValue("@FirstName", firstName);
+                        sqlCommand.Parameters.AddWithValue("@LastName", lastName);
+                        connection.Open();
+                        sqlCommand.ExecuteNonQuery();
+                        ////getAllPeople();
+                        ////clearData();
+                        ID = -1;
+                        MessageBox.Show("Name Added.");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("The operation was not completed.");
+                    }
+                    finally
+                    {
+                        // Close the connection.
+                        connection.Close();
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// DISPLAY SELECTED RECORD
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            
+            ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txtFirst.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtLast.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
+
+
+        private void updateName(string firstName, string lastName)
+        {
+            //MessageBox.Show(ID.ToString()+" <<<");
+            if (txtFirst.Text != "" && txtLast.Text != "" && ID != 0)
+
+            {
+
+                using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.PeopleConnectionString))
+                {
+                    using (SqlCommand sqlCommand = new SqlCommand("updateName", connection))
+                    {
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+                        try
+                        {
+                            sqlCommand.Parameters.AddWithValue("@ID", ID);
+                            sqlCommand.Parameters.AddWithValue("@FirstName", firstName);
+                            sqlCommand.Parameters.AddWithValue("@LastName", lastName);
+                            connection.Open();
+                            sqlCommand.ExecuteNonQuery();
+                            getAllPeople();
+                            clearData();
+                            ID = 0;
+                            MessageBox.Show("Name UPDATED.");
+                        }
+                        catch
+                        {
+                            MessageBox.Show("The operation was not completed.");
+                        }
+                        finally
+                        {
+                            // Close the connection.
+                            connection.Close();
+                        }
+                    }
+                }
+
+            }
+        }
+        //Update Record  
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            updateName(txtFirst.Text, txtLast.Text);
+        }
+        //DELETE RECORD
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            deleteName();
+        }
+
+        private void deleteName()
+        {
+            if (txtFirst.Text != "" && txtLast.Text != "" && ID != 0)
+
+            {
+
+                using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.PeopleConnectionString))
+                {
+                    using (SqlCommand sqlCommand = new SqlCommand("deleteName", connection))
+                    {
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+                        try
+                        {
+                            sqlCommand.Parameters.AddWithValue("@ID", ID);
+                            connection.Open();
+                            sqlCommand.ExecuteNonQuery();
+                            getAllPeople();
+                            clearData();
+                            MessageBox.Show("Name " + ID + " Deleted.");
+                            ID = -1;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("The operation was not completed.");
+                        }
+                        finally
+                        {
+                            // Close the connection.
+                            connection.Close();
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void dataGridView1_RowLeave(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+            //MessageBox.Show("----> " + dataGridView1.SelectedCells[0].Value.ToString());
+            //MessageBox.Show("----> " + dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString());
+            //MessageBox.Show("----> " + dataGridView1.Rows[e.RowIndex].ToString() + " --- " + dataGridView1.NewRowIndex.ToString());
+            //if (e.RowIndex == dataGridView1.NewRowIndex)
+            if(ROW != -1)
+            {
+                // user is in the new row, disable controls.
+                MessageBox.Show(ROW.ToString() + "----> " + dataGridView1.Rows[ROW].Cells[1].Value.ToString());
+                addName(dataGridView1.Rows[ROW].Cells[1].Value.ToString(), dataGridView1.Rows[ROW].Cells[2].Value.ToString());
+                ROW = -1;
+            }
+            
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex == dataGridView1.NewRowIndex)
+            {
+                //MessageBox.Show("Row Enter----> " + e.RowIndex.ToString());
+                ROW = e.RowIndex;
+            }
+            else
+            {
+                ROW = -1;
+            }
+                
+        }
+
+        public void addNameRow(string firstName, string lastName, string idStr)
+        {
+            int id;
+                
+                int.TryParse(idStr, out id);
+
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.PeopleConnectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("addNameIdx", connection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    //try
+                   // {
+                        sqlCommand.Parameters.AddWithValue("@ID", id);
+                        sqlCommand.Parameters.AddWithValue("@FirstName", firstName);
+                        sqlCommand.Parameters.AddWithValue("@LastName", lastName);
+                        connection.Open();
+                        sqlCommand.ExecuteNonQuery();
+                        getAllPeople();
+                        clearData();
+                        ID = -1;
+                        MessageBox.Show("Name Added iDX.");
+                  //  }
+                  //  catch
+                  //  {
+                 //       MessageBox.Show("The operation was not completed.");
+                 //   }
+                 //   finally
+                 //   {
+                        // Close the connection.
+                        connection.Close();
+                  //  }
+                }
+            }
+        }
     }
+
+
+
 }
+
